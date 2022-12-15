a3f7198d6d4eb8523eeb24ebb26057ad7fdcdb2b
Victor Thompson <kelechithompson26@gmail.com>
2022-12-15 13:55:00 +0100

    add user-interface

--- js/script.js
+++ js/script.js
@@ -87,3 +87,23 @@ function boldPassage(word, text) {
   return htmlString + "</p>";
 }
 
+// USER INTERFACE LOGIC //
+let offensiveWords = ["zoinks", "muppeteer", "biffaroni","loopdaloop"]
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    const boldedPassage = boldPassage(word, passage)
+    const topThreeli = top3(passage)   
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#message").text(passage)
+    $("#bolded-passage").html(boldedPassage)
+    $("#topthree").html(topThreeli)
+  });
+});
+
+
