3d2d9628f5fd18a8662d25411bfcbd3275da61b0
Victor Thompson <kelechithompson26@gmail.com>
2022-12-15 13:44:49 +0100

    add word-counter

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// BUSINESS LOGIC
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
